1bc044536bc30c627dc4b49b1b6457a20af995a1
AnoshMathew <anosh.mathew99@gmail.com>
2021-11-15 04:00:21 +0530

    first commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.scss"
+
+export default function Contact() {
+    return (
+        <div className="contact" id="contact">
+             
+
+            contact
+        </div>
+    )
+}
---
+++ src/components/testimonials/Testmonials.jsx
@@ -0,0 +1,9 @@
+import "./testmonials.scss"
+
+export default function Testmonial() {
+    return (
+        <div className="testmonials" id="testmonials">
+            testmonial
+        </div>
+    )
+}

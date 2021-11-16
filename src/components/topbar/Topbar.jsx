911e44c360452dc405fdf60e6a53422676c09b2c
AnoshMathew <anosh.mathew99@gmail.com>
2021-11-16 21:59:41 +0530

    Completed intro page

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,7 +9,7 @@ export default function Topbar({menuOpen,setMenuOpen}) {
             <div className="wrapper">
                 
                 <div className="left">
-                    <a href="#intro" className="logo">genius.</a>
+                    <a href="#intro" className="logo">Webland</a>
                     <div className="itemContainer">
                         <Person className="icon"/>
                         <span>+91 8921162428</span>

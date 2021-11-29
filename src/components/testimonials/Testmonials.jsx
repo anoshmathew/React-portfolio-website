cddbf44ad3eb8927476b601618ac52c3158ba998
AnoshMathew <anosh.mathew99@gmail.com>
2021-11-29 19:41:35 +0530

    Working on Testmonials

--- src/components/testimonials/Testmonials.jsx
+++ src/components/testimonials/Testmonials.jsx
@@ -2,10 +2,26 @@ import "./testmonials.scss"
 
 
 export default function Testmonial() {
+    
     return (
         <div className="testmonials" id="testmonials">
-            testmonial
-           
+            <h1>Testmonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/right-arrow.png" className="left" alt="" />
+                        <img className="user" src="" alt=""/>
+                        <img src="assets/youtube.png" className="right" alt=""/>
+                    </div>
+                    <div className="center">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, assumenda.
+                    </div>
+                    <div className="bottom">
+                        <h3>Alex</h3>
+                        <h4>CEO of GENex</h4>
+                    </div>
+                </div> 
+            </div>
         </div>
     )
 }

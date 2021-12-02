28bd12b6dcccc1b6f2a2d12d575706700d69b348
AnoshMathew <anosh.mathew99@gmail.com>
2021-12-02 23:31:46 +0530

    Connected to Emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,22 @@
+import emailjs from "emailjs-com"
 import { TextareaAutosize } from "@material-ui/core"
 import "./contact.scss"
 import { useState } from "react"
 
 export default function Contact() {
+
+    function sendEmail (e) {
+        e.preventDefault();
+
+    emailjs.sendForm('service_upx1hvv', 'template_4pmv48m', e.target, 'user_FiugXeIJEYQmWBaG7Gaa3')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    }
+
     const [message,setMessage] = useState(false)
     const handleSubmit = (e)=>{
         e.preventDefault();
@@ -18,11 +32,11 @@ export default function Contact() {
                  <h2>
                      Contact.
                  </h2>
-                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email"/>
-                    <textarea placeholder="Message"></textarea>
+                 <form onSubmit={handleSubmit,sendEmail}>
+                    <input type="text" placeholder="Email" name="email"/>
+                    <textarea placeholder="Message" name="message"></textarea>
                     <button type="submit">Send</button>
-                    {message && <span>Thanks, I'll replay ASAP :)</span>}
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
                  </form>
              </div>
         </div>
